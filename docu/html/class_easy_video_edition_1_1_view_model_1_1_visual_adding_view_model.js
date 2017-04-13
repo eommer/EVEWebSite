cc8d5ee9035aa702cdbf9875a9773e1345f79aee
eommer <theocharlot@hotmail.fr>
2017-04-13 03:22:48 +0200

    delete docu

--- docu/html/class_easy_video_edition_1_1_view_model_1_1_visual_adding_view_model.js
+++
@@ -1,9 +0,0 @@
-var class_easy_video_edition_1_1_view_model_1_1_visual_adding_view_model =
-[
-    [ "addPhotoCommand", "class_easy_video_edition_1_1_view_model_1_1_visual_adding_view_model.html#a59a11449687b31a3eb693cc8073f5bc2", null ],
-    [ "addVideoCommand", "class_easy_video_edition_1_1_view_model_1_1_visual_adding_view_model.html#aa1cab258672f4f32b0aa4cab66f2157d", null ],
-    [ "listPhoto", "class_easy_video_edition_1_1_view_model_1_1_visual_adding_view_model.html#a2c70bcae1aae7ed1bb752cb2bc6ac23b", null ],
-    [ "listVideo", "class_easy_video_edition_1_1_view_model_1_1_visual_adding_view_model.html#abfc77c67b6a5a6e8ed061f2b79fb6433", null ],
-    [ "mainVideo", "class_easy_video_edition_1_1_view_model_1_1_visual_adding_view_model.html#a20b0641d7eb5162843b9d88b3ae46763", null ],
-    [ "storyBoard", "class_easy_video_edition_1_1_view_model_1_1_visual_adding_view_model.html#aa6a8d44e45deba18c7f1f914d67d6045", null ]
-];
