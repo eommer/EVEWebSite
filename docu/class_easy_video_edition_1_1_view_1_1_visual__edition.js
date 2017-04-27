9a4c5bba17976a4f0cba61f9d9e5954acb6b922e
eommer <theocharlot@hotmail.fr>
2017-04-27 19:29:43 +0200

    Add files via upload

---
+++ docu/class_easy_video_edition_1_1_view_1_1_visual__edition.js
@@ -0,0 +1,6 @@
+var class_easy_video_edition_1_1_view_1_1_visual__edition =
+[
+    [ "Visual_edition", "class_easy_video_edition_1_1_view_1_1_visual__edition.html#a4f570d7cd2d8dc5084fea7282644bdd1", null ],
+    [ "InitializeComponent", "class_easy_video_edition_1_1_view_1_1_visual__edition.html#a8c6072ef47a2cda1a08f5316fcb4b4aa", null ],
+    [ "InitializeComponent", "class_easy_video_edition_1_1_view_1_1_visual__edition.html#a8c6072ef47a2cda1a08f5316fcb4b4aa", null ]
+];
---
+++ docu/interface_easy_video_edition_1_1_model_1_1_i_file.js
@@ -0,0 +1,8 @@
+var interface_easy_video_edition_1_1_model_1_1_i_file =
+[
+    [ "duration", "interface_easy_video_edition_1_1_model_1_1_i_file.html#a757e39897508078aefdcd1c0dc61625a", null ],
+    [ "fileName", "interface_easy_video_edition_1_1_model_1_1_i_file.html#a58a9e390054b89dc9aa0428cbe7a22eb", null ],
+    [ "filePath", "interface_easy_video_edition_1_1_model_1_1_i_file.html#a04ef62d5a2c4e617f9516b20fe00d114", null ],
+    [ "fileSize", "interface_easy_video_edition_1_1_model_1_1_i_file.html#a6bca50dc129201c6cfa28335fa5d4588", null ],
+    [ "miniatPath", "interface_easy_video_edition_1_1_model_1_1_i_file.html#ad53a8210cda785f18196a54a6784c586", null ]
+];
