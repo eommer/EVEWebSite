99f6e6e13d018301a5b3974c6995414ce1cb2c2b
eommer <theocharlot@hotmail.fr>
2017-04-27 19:20:34 +0200

    Add files via upload

---
+++ docu/search/all_2.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['calcduration',['calcDuration',['../class_easy_video_edition_1_1_model_1_1_video.html#a8ff4053d6f650f996560a2651e3638dc',1,'EasyVideoEdition::Model::Video']]],
+  ['calcsize',['calcSize',['../class_easy_video_edition_1_1_model_1_1_video.html#adefab990a6d97e914971723e6d208148',1,'EasyVideoEdition::Model::Video']]],
+  ['canopenfile',['canOpenFile',['../class_easy_video_edition_1_1_model_1_1_file_browser.html#a66eb9aac0ce6a622ac0801e2161fed97',1,'EasyVideoEdition::Model::FileBrowser']]],
+  ['chapters_5fadding',['Chapters_adding',['../class_easy_video_edition_1_1_view_1_1_chapters__adding.html',1,'EasyVideoEdition::View']]],
+  ['createdelegate',['CreateDelegate',['../class_xaml_generated_namespace_1_1_generated_internal_type_helper.html#a8ec4c37e82d9f4e867e9655f4eac3a78',1,'XamlGeneratedNamespace.GeneratedInternalTypeHelper.CreateDelegate(System.Type delegateType, object target, string handler)'],['../class_xaml_generated_namespace_1_1_generated_internal_type_helper.html#a8ec4c37e82d9f4e867e9655f4eac3a78',1,'XamlGeneratedNamespace.GeneratedInternalTypeHelper.CreateDelegate(System.Type delegateType, object target, string handler)']]],
+  ['createinstance',['CreateInstance',['../class_xaml_generated_namespace_1_1_generated_internal_type_helper.html#aefb7a98fceb9c287cef4756942f441d1',1,'XamlGeneratedNamespace.GeneratedInternalTypeHelper.CreateInstance(System.Type type, System.Globalization.CultureInfo culture)'],['../class_xaml_generated_namespace_1_1_generated_internal_type_helper.html#aefb7a98fceb9c287cef4756942f441d1',1,'XamlGeneratedNamespace.GeneratedInternalTypeHelper.CreateInstance(System.Type type, System.Globalization.CultureInfo culture)']]],
+  ['createsrtfile',['CreateSrtFile',['../class_easy_video_edition_1_1_model_1_1_subtitles.html#a876f297cf9842655ee6c0f18d541e747',1,'EasyVideoEdition::Model::Subtitles']]],
+  ['createsrtfilename',['CreateSrtFileName',['../class_easy_video_edition_1_1_model_1_1_subtitles.html#aa7f77c060c42ef45e003838b7220d576',1,'EasyVideoEdition::Model::Subtitles']]]
+];
---
+++ docu/search/all_9.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['main',['Main',['../class_easy_video_edition_1_1_app.html#a6690ebf2d56d23d932806e26224aeefb',1,'EasyVideoEdition.App.Main()'],['../class_easy_video_edition_1_1_app.html#a6690ebf2d56d23d932806e26224aeefb',1,'EasyVideoEdition.App.Main()']]],
+  ['mainvideo',['MainVideo',['../class_easy_video_edition_1_1_model_1_1_main_video.html',1,'EasyVideoEdition.Model.MainVideo'],['../class_easy_video_edition_1_1_view_model_1_1_visual_adding_view_model.html#a20b0641d7eb5162843b9d88b3ae46763',1,'EasyVideoEdition.ViewModel.VisualAddingViewModel.mainVideo()']]],
+  ['mainviewmodel',['MainViewModel',['../class_easy_video_edition_1_1_main_view_model.html',1,'EasyVideoEdition.MainViewModel'],['../class_easy_video_edition_1_1_view_model_1_1_main_view_model.html',1,'EasyVideoEdition.ViewModel.MainViewModel'],['../class_easy_video_edition_1_1_view_model_1_1_main_view_model.html#a94ec63dd0462bcfaa42f7a394498ddd7',1,'EasyVideoEdition.ViewModel.MainViewModel.MainViewModel()']]],
+  ['mainwindow',['MainWindow',['../class_easy_video_edition_1_1_main_window.html',1,'EasyVideoEdition']]],
+  ['miniatpath',['miniatPath',['../class_easy_video_edition_1_1_model_1_1_photo.html#af717df1851fe8969e9dc1656c9a4d645',1,'EasyVideoEdition.Model.Photo.miniatPath()'],['../class_easy_video_edition_1_1_model_1_1_video.html#a4fea03218490a1d52e05f809ba26e419',1,'EasyVideoEdition.Model.Video.miniatPath()']]]
+];
