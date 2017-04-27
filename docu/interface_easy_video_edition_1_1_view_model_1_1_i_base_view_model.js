fdcbef006db2cf23338397761fa83d7eb10768bb
eommer <theocharlot@hotmail.fr>
2017-04-27 19:31:15 +0200

    Add files via upload

---
+++ docu/interface_easy_video_edition_1_1_view_model_1_1_i_base_view_model.js
@@ -0,0 +1,4 @@
+var interface_easy_video_edition_1_1_view_model_1_1_i_base_view_model =
+[
+    [ "name", "interface_easy_video_edition_1_1_view_model_1_1_i_base_view_model.html#ab6028bdadfced31cb67538a07782c8e7", null ]
+];
---
+++ docu/namespaces.js
@@ -0,0 +1,5 @@
+var namespaces =
+[
+    [ "EasyVideoEdition", "namespace_easy_video_edition.html", "namespace_easy_video_edition" ],
+    [ "XamlGeneratedNamespace", "namespace_xaml_generated_namespace.html", null ]
+];
